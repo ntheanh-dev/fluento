be38eed8dd07420d630f47f78014ba8dc7943179
ntheanh <theanhmgt66@gmail.com>
2025-10-12 09:59:19 +0700

    Remove deprecated Docker Compose files and MySQL initialization scripts; update environment variables and configurations for backend and frontend services.

--- frontend/src/configs/oauthConfig.js
+++ frontend/src/configs/oauthConfig.js
@@ -1,5 +1,5 @@
 export const oauthConfig = {
-  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+  clientId: "940316326175-8jma9br9b2j1cjgqu992v3a693euvr6c.apps.googleusercontent.com",
   redirectUri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
-  authUri: import.meta.env.VITE_GOOGLE_AUTH_URI,
+  authUri: "https://accounts.google.com/o/oauth2/v2/auth",
 };
