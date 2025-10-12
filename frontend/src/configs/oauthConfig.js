5abcb6009ea42392029aa4b20c7f95280fe9a520
ntheanh <theanhmgt66@gmail.com>
2025-10-12 12:03:28 +0700

    add docker compose file which use remote images

--- frontend/src/configs/oauthConfig.js
+++ frontend/src/configs/oauthConfig.js
@@ -1,5 +1,5 @@
 export const oauthConfig = {
   clientId: "940316326175-8jma9br9b2j1cjgqu992v3a693euvr6c.apps.googleusercontent.com",
-  redirectUri: import.meta.env.VITE_GOOGLE_REDIRECT_URI,
+  redirectUri: "http://localhost:1234/oauth/authenticate",
   authUri: "https://accounts.google.com/o/oauth2/v2/auth",
 };
