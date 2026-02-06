22f86c080d48ed8792c61f319fcdfeb5892dca5f
ntheanh <theanhmgt66@gmail.com>
2026-02-06 17:00:08 +0700

    BE - docs: integrate swagger service

---
+++ backend/src/main/java/com/nta/configuration/OpenApiDocConfig.java
@@ -0,0 +1,36 @@
+package com.nta.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiDocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .addServersItem(new Server().url("http://localhost:8080/api").description("Local"))
+                .addServersItem(new Server().url("https://api.fluento.com").description("Production"))
+                .info(new Info()
+                        .title("Fluento API")
+                        .version("v1.0")
+                        .description("Fluento Application APIs")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
